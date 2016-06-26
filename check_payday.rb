57fb6c2e9f772461485ad18733434e1b7fced014
Lukas <Lukasbarry@yahoo.com>
2016-06-26 13:28:54 -0400

    check payday

---
+++ check_payday.rb
@@ -0,0 +1,18 @@
+#simple program to check if payday is coming up
+
+days_in_pay_period = 14
+seconds_in_a_day = 60 * 60 * 24
+
+matching_date = Time.local(0, 0, 0, 26, 6, 2016, 7, 178, true, "EDT")
+current_date = Time.new()
+
+difference_in_seconds = (current_date - matching_date)
+difference_in_days = (difference_in_seconds / seconds_in_a_day).to_i
+days_to_wait = (days_in_pay_period - difference_in_days) % days_in_pay_period
+
+if (days_to_wait.zero?)
+  puts "Payday Today"
+else
+  print "Payday in " + days_to_wait.to_s + " day"
+  puts days_to_wait == 1 ? '.' : 's.'
+end
