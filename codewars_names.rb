7d175679eeed7957f62809027ef4c32055cfdb8b
Lukas <Lukasbarry@yahoo.com>
2016-07-02 10:36:24 -0400

    combining array of names

--- codewars_names.rb
+++ codewars_names.rb
@@ -1,3 +1,10 @@
-def list names
-  
+def list
+  @array = ['Bart', 'Maggie', 'Lisa', 'Carolyn']
+  @separator1 = ', '
+  @separator2 = ', and '
+  @last_word = @array[-1]
+  @array.pop
+  @array.join(@separator1) + @separator2 + @last_word
 end
+
+puts list
